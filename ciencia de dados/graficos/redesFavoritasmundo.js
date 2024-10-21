60bf422e72da31675b1053ca68742af3fa22a8ea
zp06 <108944921+zp06@users.noreply.github.com>
2024-10-21 08:06:27 -0300

    lobao.

---
+++ ciencia de dados/graficos/redesFavoritasmundo.js
@@ -0,0 +1,45 @@
+import { getCSS } from "./common.js"
+
+async function redesFavoritasMundo() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const redes = Object.keys(dados)
+    const valores = Object.values(dados)
+
+    const data = [
+        {
+            values: valores,
+            labels: redes,
+            type: 'pie',
+            textinfo: 'label+percent'
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Redes sociais que os usuários mais gostam',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30
+            }
+        },
+        legend: {
+            font: {
+                color: getCSS('--primary-color'),
+                size: 16
+            }
+        }
+    }
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
+}
+
+redesFavoritasMundo()
